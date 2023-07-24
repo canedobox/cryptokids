5d047198edb3233121c8682b86c05c1660c6571d
Luiz Canedo <65631733+canedobox@users.noreply.github.com>
2023-07-24 17:00:21 +0100

    Save contract address and ABI files on deployment

--- backend/scripts/deploy.js
+++ backend/scripts/deploy.js
@@ -1,11 +1,16 @@
-const { ethers, network } = require("hardhat");
+const { ethers, network, artifacts } = require("hardhat");
+const fs = require("fs");
+const path = require("path");
 
+// Set constants for contract details.
+const CONTRACT_NAME = "CryptoKids";
+const TOKEN_NAME = "CryptoKids";
+const TOKEN_SYMBOL = "CK";
+
+/**
+ * Main.
+ */
 async function main() {
-  // Set contract details.
-  const CONTRACT_NAME = "CryptoKids";
-  const TOKEN_NAME = "CryptoKids";
-  const TOKEN_SYMBOL = "CK";
-
   // Deploy contract.
   const contract = await ethers.deployContract(CONTRACT_NAME, [
     TOKEN_NAME,
@@ -17,9 +22,62 @@ async function main() {
   console.log(
     `${CONTRACT_NAME} smart contract deployed to the ${network.name} network.`
   );
+  console.log(`Network ChainID: ${network.config.chainId}`);
   console.log(`Contract address: ${contract.target}`);
   console.log(`Token name: ${TOKEN_NAME}`);
   console.log(`Token symbol: ${TOKEN_SYMBOL}`);
+
+  // Save contract files.
+  saveContractFiles(contract);
+}
+
+/**
+ * Save contract address and ABI files in the frontend directory.
+ */
+function saveContractFiles(contract_) {
+  // Set frontend directory path.
+  const frontendDirPath = path.join(__dirname, "../../src/contracts");
+
+  // Create directory if it does not exist.
+  if (!fs.existsSync(frontendDirPath)) {
+    fs.mkdirSync(frontendDirPath);
+  }
+
+  /***** CONTRACT ABI *****/
+  // Get contract artifacts.
+  const artifact = artifacts.readArtifactSync(CONTRACT_NAME);
+
+  // Save contract ABI file.
+  fs.writeFileSync(
+    path.join(frontendDirPath, `${CONTRACT_NAME}-abi.json`),
+    JSON.stringify(artifact.abi, null, 2)
+  );
+
+  /***** CONTRACT ADDRESS *****/
+  // Set address file path.
+  const addressFilePath = path.join(
+    frontendDirPath,
+    `${CONTRACT_NAME}-address.json`
+  );
+
+  // Create address file variable.
+  let addressFile = {};
+  // Get address file if it exists.
+  if (fs.existsSync(addressFilePath)) {
+    addressFile = require(addressFilePath);
+  }
+
+  // Store contract address using the network chain ID.
+  addressFile[network.config.chainId] = {
+    name: network.name,
+    address: contract_.target
+  };
+
+  // Save contract address file.
+  fs.writeFileSync(
+    path.join(addressFilePath),
+    JSON.stringify(addressFile, null, 2)
+  );
 }
 
 /**
