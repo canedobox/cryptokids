c045df8bd414ba6748048782f40477b42556b120
Luiz Canedo <65631733+canedobox@users.noreply.github.com>
2023-08-10 12:46:12 +0100

    Deployed smart contract to Sepolia

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,13 +1,8 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env.local", override: true });
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  networks: {
-    localhost: {
-      url: "http://127.0.0.1:8545",
-      chainId: 31337
-    }
-  },
   solidity: {
     version: "0.8.19",
     settings: {
@@ -16,5 +11,19 @@ module.exports = {
         runs: 200
       }
     }
+  },
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      chainId: 31337
+    },
+    sepolia: {
+      chainId: 11155111,
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [process.env.ACCOUNT_PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
   }
 };
